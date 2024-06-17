af3d76b1ed347630a969197461ebdd04ee1158ef
Mabende <belindamabende@gmail.com>
2024-06-17 05:06:10 +0200

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/06/17.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
